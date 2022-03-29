0bff23dc6dc461cf754222b48d112c1b4a8150d5
TopGunSnake <tjwebster@crimson.ua.edu>
2022-03-29 07:58:02 -0500

    Clened up target number construction

--- fo-fdc-comms/src/message_to_observer.rs
+++ fo-fdc-comms/src/message_to_observer.rs
@@ -20,14 +20,14 @@ pub struct TargetNumber {
 impl TargetNumber {
     /// Constructs a new TargetNumber instance, returning an error if the input does not match target number standards:
     /// [A-Z]{2}\d{4}
-    pub fn new(input: &str) -> Result<Self, ()> {
-        let regex_pattern: Regex = regex::Regex::new(r"[A-Z]{2}\d{4}").unwrap();
+    pub fn new(input: &str) -> Result<Self, &str> {
+        let regex_pattern: Regex = regex::Regex::new(r"^[A-Z]{2}\d{4}$").unwrap();
         if regex_pattern.is_match(input) {
             Ok(Self {
                 value: input.to_string(),
             })
         } else {
-            Err(())
+            Err("Invalid target number")
         }
     }
 }
