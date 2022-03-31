dada75291bee1146052c2b9424a0e0203933a74e
TopGunSnake <tjwebster@crimson.ua.edu>
2022-03-30 21:09:27 -0500

    Fixed a hyphen to an underscore

--- doc-gen.ps1
+++ doc-gen.ps1
@@ -1,4 +1,4 @@
 cargo doc --workspace --no-deps
 Remove-Item '.\docs\' -Recurse
-Write-Output "<meta http-equiv=`"refresh`" content=`"0; url=example-simulators`">" > target/doc/index.html
+Write-Output "<meta http-equiv=`"refresh`" content=`"0; url=example_simulators`">" > target\doc\index.html
 Copy-Item target\doc .\docs -Recurse
