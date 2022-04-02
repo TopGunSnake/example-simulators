702811d888082b976a6eb16a1137f4cf18995b22
TopGunSnake <tjwebster@crimson.ua.edu>
2022-04-02 14:30:32 -0500

    First pass at the UDP send/recv tasks

--- fo_sim/src/fo_fdc_commhandler.rs
+++ fo_sim/src/fo_fdc_commhandler.rs
@@ -1,11 +1,20 @@
 //! Contains the message types that the FO sim can send/receive, as well as the communication tasks (send and receive)
 //!
+use std::sync::Arc;
+
+use anyhow::Result;
 use fo_fdc_comms::{
     battle_damage_assessment::BattleDamageAssessment,
     message_to_observer::MessageToObserver,
     readback::SolidReadback,
     request_for_fire::WarnOrder,
     shot_fire::{RoundsComplete, Shot, Splash},
+};
+use serde_json::Value;
+use tokio::{
+    join,
+    net::UdpSocket,
+    sync::mpsc::{UnboundedReceiver, UnboundedSender},
 };
 
 /// Represents messages that can be received from the FDC
@@ -44,4 +53,90 @@ pub(crate) enum ToFdcMessage {
     /// Indicates a SolidReadback (A message was confirmed as readback correctly)
     /// Primarily used for the readback of a RRF or BDA
     SolidReadback(SolidReadback),
+}
+
+/// Provides a reader/writer loop, handling writes from the
+pub(crate) async fn fo_fdc_commhandler_loop(
+    to_fdc: UnboundedReceiver<ToFdcMessage>,
+    from_fdc: UnboundedSender<FromFdcMessage>,
+) -> Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:8080").await?;
+    socket.connect("127.0.0.1:8081").await?;
+
+    // Spin off listener thread
+    let socket = Arc::new(socket);
+    let recv_handle = {
+        let socket = Arc::clone(&socket);
+        tokio::spawn(async move { recv_loop(from_fdc, socket).await })
+    };
+
+    // Spin off writer thread
+    let send_handle = tokio::spawn(async move { send_loop(to_fdc, socket).await });
+
+    let _ = join!(recv_handle, send_handle);
+
+    Ok(())
+}
+
+async fn recv_loop(
+    from_fdc_sender: UnboundedSender<FromFdcMessage>,
+    socket: Arc<UdpSocket>,
+) -> Result<()> {
+    let mut buffer = Vec::with_capacity(1024);
+
+    while let Ok(_bytes_read) = socket.recv(&mut buffer).await {
+        let value: Value = serde_json::from_slice(&buffer)?;
+
+        let result = match value {
+            // Incoming Request for Fire Confirmation
+            Value::Object(map) if map.contains_key("mission_type") => Ok(
+                FromFdcMessage::RequestForFireConfirm(serde_json::from_slice(&buffer)?),
+            ),
+            // Incoming Message to Observer
+            Value::Object(map) if map.contains_key("") => Ok(FromFdcMessage::MessageToObserver(
+                serde_json::from_slice(&buffer)?,
+            )),
+            // Incoming Shot
+            Value::Object(map) if map.contains_key("") => {
+                Ok(FromFdcMessage::Shot(serde_json::from_slice(&buffer)?))
+            }
+            // Incoming Splash
+            Value::Object(map) if map.contains_key("") => {
+                Ok(FromFdcMessage::Splash(serde_json::from_slice(&buffer)?))
+            }
+            // Incoming Rounds Complete
+            Value::Object(map) if map.contains_key("") => Ok(FromFdcMessage::RoundsComplete(
+                serde_json::from_slice(&buffer)?,
+            )),
+            // Incoming Solid Readback
+            Value::Object(map) if map.contains_key("") => Ok(FromFdcMessage::SolidReadback(
+                serde_json::from_slice(&buffer)?,
+            )),
+            _ => Err(std::io::Error::from(std::io::ErrorKind::InvalidData)),
+        }?;
+
+        from_fdc_sender.send(result)?;
+    }
+    Ok(())
+}
+
+async fn send_loop(
+    mut to_fdc_receiver: UnboundedReceiver<ToFdcMessage>,
+    socket: Arc<UdpSocket>,
+) -> Result<()> {
+    while let Some(message_to_fdc) = to_fdc_receiver.recv().await {
+        let bytes = match message_to_fdc {
+            ToFdcMessage::RequestForFire(msg) => serde_json::to_vec(&msg)?,
+            ToFdcMessage::MessageToObserverConfirm(msg) => serde_json::to_vec(&msg)?,
+            ToFdcMessage::ShotConfirm(msg) => serde_json::to_vec(&msg)?,
+            ToFdcMessage::SplashConfirm(msg) => serde_json::to_vec(&msg)?,
+            ToFdcMessage::RoundsCompleteConfirm(msg) => serde_json::to_vec(&msg)?,
+            ToFdcMessage::BattleDamageAssessment(msg) => serde_json::to_vec(&msg)?,
+            ToFdcMessage::SolidReadback(msg) => serde_json::to_vec(&msg)?,
+        };
+
+        socket.send(&bytes).await?;
+    }
+
+    Ok(())
 }
--- fo_sim/src/main.rs
+++ fo_sim/src/main.rs
@@ -3,12 +3,14 @@
 //! This crate provides an `async` FO Simulator built on `tokio`.
 //! The crate will listen for and talk to any FDC over the [`fo_fdc_comms`] message interface.
 use anyhow::Result;
-use state_machine::state_machine_loop;
 use tokio::{select, sync::mpsc, try_join};
 use tracing::info;
 
 mod fo_fdc_commhandler;
 mod state_machine;
+
+use fo_fdc_commhandler::fo_fdc_commhandler_loop;
+use state_machine::state_machine_loop;
 
 /// Entry function
 #[tokio::main]
@@ -18,6 +20,11 @@ async fn main() -> Result<()> {
     let (message_queue_sender, message_queue) = mpsc::unbounded_channel();
     let (to_fdc, to_fdc_receiver) = mpsc::unbounded_channel();
 
+    info!("Starting the FO-FDC Comm Handler...");
+    let fo_fdc_commhandler_handle = tokio::spawn(async move {
+        fo_fdc_commhandler_loop(to_fdc_receiver, message_queue_sender).await
+    });
+
     info!("Starting state machine...");
     let state_machine_handle =
         tokio::spawn(async move { state_machine_loop(message_queue, to_fdc).await });
@@ -26,7 +33,7 @@ async fn main() -> Result<()> {
     select! {
         _ = tokio::signal::ctrl_c() => {state_machine_handle.abort()}
     }
-    let results = try_join!(state_machine_handle);
+    let results = try_join!(fo_fdc_commhandler_handle, state_machine_handle);
 
     //TODO: Unpack the results to propagate relevant errors.
     Ok(())
